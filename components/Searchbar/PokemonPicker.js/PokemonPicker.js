ed3f58af450475a4e728ec8eb940e805e9178f2d
EivindYN <eivindyunilsen@hotmail.com>
2019-11-15 12:33:53 +0100

    Issue #21. Fix picker for IOS

--- components/Searchbar/PokemonPicker.js/PokemonPicker.js
+++ components/Searchbar/PokemonPicker.js/PokemonPicker.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Picker, Text, StyleSheet, View } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import { fireAction } from '../../../redux/ducks/sortDuck';
+import RNPickerSelect from 'react-native-picker-select';
 
 export default function PokemonPicker() {
   const dispatch = useDispatch();
@@ -23,17 +24,26 @@ export default function PokemonPicker() {
     <View style={styles.pokemonPickerContainer}>
       <Text style={styles.pokemonPickerTitle}>Sort by:</Text>
       <View style={styles.pokemonPicker}>
-        <Picker
-          onValueChange={value => handleOptionSelect(value)}
-          selectedValue={
-            sortInfo.sortBy + ' ' + (sortInfo.ascending ? 'asc' : 'desc')
-          }
-        >
-          <Picker.Item label='Lowest to highest id' value='id asc' />
-          <Picker.Item label='Highest to lowest id' value='id desc' />
-          <Picker.Item label='A to Z' value='name asc' />
-          <Picker.Item label='Z to A' value='name desc' />
-        </Picker>
+        <View style={styles.pokemonPicker}>
+          <RNPickerSelect
+            style={{
+              inputIOS: {
+                padding: 20
+              }
+            }}
+            onValueChange={value => handleOptionSelect(value)}
+            value={
+              sortInfo.sortBy + ' ' + (sortInfo.ascending ? 'asc' : 'desc')
+            }
+            placeholder={{}}
+            items={[
+              { label: 'Lowest to highest id', value: 'id asc' },
+              { label: 'Highest to lowest id', value: 'id desc' },
+              { label: 'A to Z', value: 'name asc' },
+              { label: 'Z to A', value: 'name desc' }
+            ]}
+          />
+        </View>
       </View>
     </View>
   );
